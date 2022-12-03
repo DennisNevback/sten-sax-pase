c06ecada00afb4099fef0c005d172010faf0b747
D <dennis.nevback@gmail.com>
2022-12-03 19:03:51 +0100

    Added basic re

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+import re
 playing = True
+player1_move = ''
+player2_move = ''
 
 
 def restart():
     global playing
     game_state = input('Would you like to play again? (Y/N)')
+    game_state = game_state.upper()
     if game_state == 'N':
         playing = False
+        print('Thank you for playing!')
 
 
-while playing:
-    player1_move = input(
-        'Player one please choose - Rock(R), Paper(P) or Scissor(S)?')
-    player2_move = input(
-        'Player two please choose - Rock(R), Paper(P) or Scissor(S)?')
+def game_loop():
     if player1_move == 'R' and player2_move == 'R':
         print('it\'s a tie! Another round!')
     elif player1_move == 'R' and player2_move == 'S':
@@ -37,3 +38,20 @@ while playing:
         restart()
     elif player1_move == 'S' and player2_move == 'S':
         print('it\'s a tie! Another round!')
+
+
+while playing:
+    player1_move = input(
+        'Player one please choose - Rock(R), Paper(P) or Scissor(S)?\n')
+    player1_move = player1_move.upper()
+    if not re.match('^[RPS]$', player1_move):
+        print('Please input either R, P Or S')
+    else:
+        player2_move = input(
+            'Player two please choose - Rock(R), Paper(P) or Scissor(S)?\n')
+        player2_move = player2_move.upper()
+        if not re.match('^[RPS]$', player2_move):
+            print('Please input either R, P Or S\n Game restarting')
+
+        else:
+            game_loop()
