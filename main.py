c8f4d0b57118d1d47fa5f80c542c2768f7bd048a
D <dennis.nevback@gmail.com>
2022-12-03 17:14:15 +0100

    Basic game function

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 playing = True
+
+
+def restart():
+    global playing
+    game_state = input('Would you like to play again? (Y/N)')
+    if game_state == 'N':
+        playing = False
+
+
+while playing:
+    player1_move = input(
+        'Player one please choose - Rock(R), Paper(P) or Scissor(S)?')
+    player2_move = input(
+        'Player two please choose - Rock(R), Paper(P) or Scissor(S)?')
+    if player1_move == 'R' and player2_move == 'R':
+        print('it\'s a tie! Another round!')
+    elif player1_move == 'R' and player2_move == 'S':
+        print('Player one wins!')
+        restart()
+    elif player1_move == 'R' and player2_move == 'P':
+        print('Player two wins!')
+        restart()
+    elif player1_move == 'P' and player2_move == 'R':
+        print('Player one wins!')
+        restart()
+    elif player1_move == 'P' and player2_move == 'P':
+        print('it\'s a tie! Another round!')
+    elif player1_move == 'P' and player2_move == 'S':
+        print('Player two wins!')
+        restart()
+    elif player1_move == 'S' and player2_move == 'R':
+        print('Player two wins!')
+        restart()
+    elif player1_move == 'S' and player2_move == 'P':
+        print('Player one wins!')
+        restart()
+    elif player1_move == 'S' and player2_move == 'S':
+        print('it\'s a tie! Another round!')
